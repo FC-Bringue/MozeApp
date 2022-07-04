142113619effb2c2cf6a22b21ec41fc99dee8763
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2022-07-04 22:30:26 +0200

    working

--- assets/app.js
+++ assets/app.js
@@ -4,17 +4,9 @@
  * We recommend including the built version of this JavaScript file
  * (and its CSS file) in your base layout (base.html.twig).
  */
-// any CSS you import will output into a single css file (app.css in this case)
-// import './styles/app.css';
-import '.app.scss';
 
-import * as bootstrap from 'bootstrap';
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.css';
 
 // start the Stimulus application
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-// import index.tsx
-import './index.tsx';
+import './bootstrap';
