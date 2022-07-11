71685e9bb1992e719215a0cdc217de8fbbf3fd25
Hiteazel <khacminhbrandon.vo@gmail.com>
2022-07-11 10:02:08 +0200

    début bouton

--- assets/js/components/settings/Application.tsx
+++ assets/js/components/settings/Application.tsx
@@ -1,9 +1,35 @@
 import amazonMusic from "../../../img/logos/amazonMusic.png";
 import spotify from "../../../img/logos/spotify.png";
 import deezer from "../../../img/logos/deezer.png";
+import React, {useState, useEffect} from "React";
+import { useSelector } from "react-redux";
 
 import "../../../styles/settings/Parametres.css";
 import "../../../styles/settings/Application.css";
+
+function isConnected() {
+
+  const [isConnected, setIsConnected] = useState([]);
+
+  useEffect(() => {
+    const Bearer = useSelector((state: any) => state.userInfos.token);
+    const headers ={
+      "Content-Type": "application/json",
+    }
+    fetch("/api/isConnected")
+      .then(res => res.json())
+      .then(data => setIsConnected(data))
+      .catch(err => console.log(err));
+  }, [isConnected]);
+
+  return<>
+    {
+      isConnected.map(post => {
+
+      })
+    }
+  </>
+}
 
 const Application = () => {
   return (
@@ -12,18 +38,6 @@ const Application = () => {
         <p>CONNECTER SON COMPTE</p>
         <div className="appLogo">
           <img src={spotify} title="spotify" />
-        </div>
-      </div>
-      <div className="deezer">
-        <p>CONNECTER SON COMPTE</p>
-        <div className="appLogo">
-          <img src={deezer} title="deezer" />
-        </div>
-      </div>
-      <div className="amazon">
-        <p>CONNECTER SON COMPTE</p>
-        <div className="appLogo">
-          <img src={amazonMusic} title="amazonMusic" />
         </div>
       </div>
     </section>
