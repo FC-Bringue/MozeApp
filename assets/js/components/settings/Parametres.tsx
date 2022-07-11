11966d5005623ff26f5d115048faf0b3f81a9585
deyja8 <72979041+deyja8@users.noreply.github.com>
2022-07-11 17:07:51 +0200

    Add General.tsx

--- assets/js/components/settings/Parametres.tsx
+++ assets/js/components/settings/Parametres.tsx
@@ -1,17 +1,28 @@
 import {
   Outlet,
-  Route,
-  Routes,
+  useLocation,
   useNavigate,
   useParams,
 } from "react-router-dom";
+import General from "./General"
 import "../../../styles/settings/Parametres.css";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const Parametres = () => {
+
+  const location = useLocation();
+
+  console.log('hash', location.hash);
+  console.log('pathname', location.pathname);
+  console.log('search', location.search);
   const navigate = useNavigate();
   let { subTab } = useParams();
   const barname = useSelector((state: any) => state.userInfos.name);
+
+  useEffect( () => {
+      navigate("/dashboard/settings/general")
+  }, [])
 
   console.log("subTab", subTab);
 
@@ -20,13 +31,13 @@ const Parametres = () => {
       <h4>PARAMETRES</h4>
       <h1>{barname && barname}</h1>
       <div className="navigation">
-        <div className={subTab === "general" ? "param-nav-active" : ""}
-            onClick={() => navigate("settings/general")}
+        <div className={location.pathname === "/dashboard/settings/general" ? "param-nav-active" : ""}
+            onClick={() => navigate("general")}
         >
           <p>GENERAL</p>
         </div>
         <div
-          className={subTab === "linked-apps" ? "param-nav-active" : ""}
+          className={location.pathname === "/dashboard/settings/linked-apps" ? "param-nav-active" : ""}
           onClick={() => navigate("linked-apps")}
         >
           <p>APPLICATIONS</p>
